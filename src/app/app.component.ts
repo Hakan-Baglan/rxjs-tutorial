2eda1bcfec2842ed44af70a16ca359874a1e9aa5
Hakan Baglan <baglanhakan42@gmail.com>
2022-01-17 19:24:43 +0300

    Subjet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
+import { AsyncSubject, BehaviorSubject, Observable, ReplaySubject, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,9 +9,9 @@ import { Observable, Subject } from 'rxjs';
 export class AppComponent implements OnInit {
   title = 'rxjs-tutorial';
   ngOnInit(): void {
-    this.metod1();
+    // this.metod1();
     this.metod2();
-    
+
   }
   // ------------- Observable-------------------
   metod1() {
@@ -24,7 +24,7 @@ export class AppComponent implements OnInit {
       data.next(1);
       data.next(3);
       data.next(5);
-      // datanın sona erdiğini bimek istiyorsak complete() kulanıyoruz
+      // datanın sona erdiğini bilmek istiyorsak complete() kulanıyoruz
       // data sona erdiğinde başka bir data vermiyeceksek complete() kullanıyoruz.
       data.complete()
     });
@@ -50,17 +50,39 @@ export class AppComponent implements OnInit {
     // observable.subscribe((data:any)=>{
     //   console.log(data);
     // })
-
-
-
+    //ornek:
   }
 
-  //  --------------------Subject---------------------------
-  metod2(){
+ 
+  metod2() {
+     //  --------------------Subject---------------------------
     // Bir observable a birden fazla observer tüketiyorsa buna Subject denir.
-    const subject = new Subject() 
+
+    // let data: any = "Hilmi";
+    // const subject = new Subject();
+    // const subject = new BehaviorSubject(data);
+    // const subject = new ReplaySubject(2);
+    const subject = new AsyncSubject();
+    subject.subscribe(data=> console.log(`ObservableA ${data}`))
+    subject.subscribe(data=> console.log(`ObservableB ${data}`))
+    subject.next(3);
+    subject.next(5);
+    subject.subscribe(data=> console.log(`ObservableC ${data}`))
+    subject.next(7);
+    subject.next(9);
+    subject.complete();
+    subject.next("Ahmet");
+    //  --------------------Subject-Türleri---------------------------
+    // 1- BehavirSubject
+    // 2- ReplaySubject
+    // 3- AsyncSubject
+    // 1- BehavirSubject = akışa subscribe olursa bir önceki değerden başlar.
+    // 2- ReplaySubject = akışa subscribe olunan dataya ne kadar 
+    // 3- AsyncSubject = akıştaki son değerin alınabilmesi için kullanılan Subject türüdür
+    //    akışta sonuncu veriyi anlayabimek için complate() fonksiyonunun tetiklenmesini bekler
+    //    bu durumda sadece 9 gelir akıştan.
   }
-  
+
 
 
 }
